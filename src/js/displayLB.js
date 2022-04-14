0bfc76f169253f735fedd857b40b43927f8fe6e7
melaku <melakushiferaw05@gmail.com>
2022-04-14 10:04:09 +0300

    added Api features

--- src/js/displayLB.js
+++ src/js/displayLB.js
@@ -1,36 +1,15 @@
-const scoreArr = [
-  {
-    name: 'melaku',
-    score: 96,
-  },
-  {
-    name: 'shiferaw',
-    score: 99,
-  },
-  {
-    name: 'Eshetu',
-    score: 93,
-  },
-];
-
-const tableData = (item) => {
+const tableData = (user, score) => {
   const tableBD = document.querySelector('#table-body');
   const tableRow = document.createElement('tr');
   const tableTD1 = document.createElement('td');
   const tableTD2 = document.createElement('td');
 
-  tableTD1.innerHTML = item.name;
-  tableTD2.innerHTML = item.score;
+  tableTD1.innerHTML = `${user}`;
+  tableTD2.innerHTML = `${score}`;
 
   tableRow.appendChild(tableTD1);
   tableRow.appendChild(tableTD2);
 
   tableBD.appendChild(tableRow);
 };
-
-const displayScoreArr = () => {
-  scoreArr.forEach((item) => {
-    tableData(item);
-  });
-};
-export default displayScoreArr();
+export default tableData;
